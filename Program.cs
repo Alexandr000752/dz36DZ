67f49d165372d17b43167f10ce2965c7da6c645a
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-19 20:08:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Write("Введите номер: ");
+int numberA = int.Parse(Console.ReadLine());
+
+int[] massiv = new int[numberA];
+
+for(int i = 0; i< massiv.Length; i++)
+{
+	massiv[i] = new Random().Next(1, 100);
+	Console.Write(massiv[i] + ", ");
+}
+
+
+PrintNeChet(massiv);
+
+void PrintNeChet(int[] array)
+{
+	int result = 0;
+	for(int index = 1; index < array.Length; index = index + 2)
+	{
+		result = result + array[index];
+	}
+	Console.WriteLine("Сумма всех нечетных позиций будет ровна: " + result);
+}
+
+
